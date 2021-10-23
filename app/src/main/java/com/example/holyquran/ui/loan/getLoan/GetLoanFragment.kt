adab688dcf459ced0b8e1c0ab826deef21ffb0ed
AmirHossein <amirrajaezadeh1383@gmail.com>
2021-10-23 11:43:14 +0330

    fixed comma issue in both eng/pers languages

--- app/src/main/java/com/example/holyquran/ui/loan/getLoan/GetLoanFragment.kt
+++ app/src/main/java/com/example/holyquran/ui/loan/getLoan/GetLoanFragment.kt
@@ -4,6 +4,7 @@ import NumberTextWatcherForThousand
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -30,6 +32,7 @@ class GetLoanFragment : Fragment(), AdapterView.OnItemSelectedListener {
     lateinit var mGetLoanBinding: FragmentGetLoanBinding
     lateinit var mGetLoanViewModel: GetLoanViewModel
     var id: Long = 0L
+    var checkComma = false
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -74,15 +77,22 @@ class GetLoanFragment : Fragment(), AdapterView.OnItemSelectedListener {
         }
 
         mGetLoanViewModel.getLoan.observe(viewLifecycleOwner, Observer {
-            val removeSectionComma = mGetLoanBinding.sectionPayment.text.toString().replace(",", "")
             mGetLoanBinding.loanAmount.addTextChangedListener(
                 NumberTextWatcherForThousand(
                     mGetLoanBinding.loanAmount
                 )
             )
-            val removeComma =
-                NumberTextWatcherForThousand.trimCommaOfString(mGetLoanBinding.loanAmount.text.toString())
-                    .replace(",", "")
+            val string: String = mGetLoanBinding.sectionPayment.text.toString()
+            val english: Boolean = "," in string
+            val persian: Boolean = "٬" in string
+            Log.d("TAG", "onCreateView: $english,$persian")
+
+            if (english) {
+                checkComma = false
+            } else if (persian) {
+                checkComma = true
+            }
+
             if (it == true) {
                 if (mGetLoanBinding.loanAmount.text?.let { it1 ->
                         mGetLoanBinding.loanSections.text.isEmpty()
@@ -91,19 +101,16 @@ class GetLoanFragment : Fragment(), AdapterView.OnItemSelectedListener {
                     } == true
                 ) {
                 } else {
-                    mGetLoanViewModel.insertLoanTimeSpinner(
-                        removeComma,
-                        mGetLoanBinding.loanDate.text.toString(),
-                        mGetLoanBinding.loanSections.text.toString(),
-//                        mGetLoanBinding.spinner.selectedItem.toString(),
-                        removeSectionComma,
-                        id
-                    )
+
+                    if (checkComma) {
+                        insertDataPersianComma()
+                    } else {
+                        insertDataEnglishComma()
+                    }
                     requireView().findNavController().popBackStack()
                 }
             }
         })
-
         val pdate = PersianDate()
         val pdformater1 = PersianDateFormat("Y/m/d")
         pdformater1.format(pdate) //1396/05/20
@@ -154,6 +161,7 @@ class GetLoanFragment : Fragment(), AdapterView.OnItemSelectedListener {
                 count: Int, after: Int
             ) {
             }
+
             override fun onTextChanged(
                 s: CharSequence, start: Int,
                 before: Int, count: Int
@@ -194,50 +202,40 @@ class GetLoanFragment : Fragment(), AdapterView.OnItemSelectedListener {
             mGetLoanBinding.totalLoan.text = NumberFormat.getInstance().format(result3)
         }
     }
+
+    fun insertDataEnglishComma() {
+        val removeComma =
+            NumberTextWatcherForThousand.trimCommaOfString(mGetLoanBinding.loanAmount.text.toString())
+                .replace(",", "")
+
+        val removeCommaSectionEnglish =
+            mGetLoanBinding.sectionPayment.text.toString().replace(",", "")
+
+        mGetLoanViewModel.insertLoanTimeSpinner(
+            removeComma,
+            mGetLoanBinding.loanDate.text.toString(),
+            mGetLoanBinding.loanSections.text.toString(),
+            removeCommaSectionEnglish,
+            id
+        )
+    }
+
+    fun insertDataPersianComma() {
+        val removeComma =
+            NumberTextWatcherForThousand.trimCommaOfString(mGetLoanBinding.loanAmount.text.toString())
+                .replace(",", "")
+
+        val removeCommaSectionFarsi =
+            mGetLoanBinding.sectionPayment.text.toString().replace("٬", "")
+
+        mGetLoanViewModel.insertLoanTimeSpinner(
+            removeComma,
+            mGetLoanBinding.loanDate.text.toString(),
+            mGetLoanBinding.loanSections.text.toString(),
+            removeCommaSectionFarsi,
+            id
+        )
+    }
+
+
 }
-//mGetLoanViewModel.userName.observe(viewLifecycleOwner, {
-//    if (it != null) {
-//        mGetLoanBinding.userName = it
-//    }
-//    Toast.makeText(
-//        activity,
-//        "  وام با موفقیت برای کاربر${it.fullName} ثبت شد ",
-//        Toast.LENGTH_LONG
-//    ).show()
-//    this.findNavController().navigate(
-//        GetLoanFragmentDirections.actionGetLoanFragmentToIncreaseMoneyFragment(
-//            id
-//        )
-//    )
-//})
-
-
-//        val benefitPresent: String = mGetLoanBinding.spinner.getSelectedItem().toString()
-//            val division = "/"
-//            val time = "*"
-//            val minus = "-"
-//            val plus = "+"
-//            val loanAmount = mGetLoanBinding.loanAmount.text.toString()
-//            val loanSections = mGetLoanBinding.loanSections.text.toString()
-//            val benefitPresent = mGetLoanBinding.benefitPrecent.text.toString()
-//
-//            val loanBenefit =
-//                loanSections + time + benefitPresent + time + loanAmount + division + 2400
-//            val test = loanBenefit + plus + loanAmount
-//            val lastResult = test + division + 12
-//
-//            Log.d("TAG", "calculateTest: $lastResult")
-//            try {
-//                val expression = ExpressionBuilder(lastResult).build()
-//                val result = expression.evaluate()
-//                val longResult = result.toLong()
-//                if (result == longResult.toDouble())
-//                    mGetLoanBinding.sectionPrice.text = longResult.toString()
-//                else
-//                    Toast.makeText(activity, "$result", Toast.LENGTH_LONG).show()
-//
-//                mGetLoanBinding.sectionPrice.text = result.toString()
-//
-//            } catch (e: Exception) {
-//                Log.d("Exception", " message : " + e.message)
-//            }
--- app/src/main/java/com/example/holyquran/ui/payPayments/PayPaymentsFragment.kt
+++ app/src/main/java/com/example/holyquran/ui/payPayments/PayPaymentsFragment.kt
@@ -4,12 +4,14 @@ import NumberTextWatcherForThousand
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
@@ -39,7 +41,6 @@ class PayPaymentsFragment : Fragment() {
                 container,
                 false
             )
-
         val application = requireNotNull(this.activity).application
         val userDAO = UserDatabase.getInstance(application).mUserDAO
         val transactionDAO = UserDatabase.getInstance(application).mTransactionsDAO
@@ -182,7 +183,6 @@ class PayPaymentsFragment : Fragment() {
                     mPayPaymentsBinding.paymentsLayout.getEditText()?.setText(getPaymentPrice);
                 }
             })
-
         return mPayPaymentsBinding.root
     }
 
